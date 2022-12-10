a4485a258a743b4dfb47ee6181e38d39a2fb5423
Jago de Vreede <jagodevreede@gmail.com>
2022-12-10 13:05:57 +0100

    chore: change toplevel package to io.github

---
+++ semver-check-maven-plugin-example/src/test/java/io/github/jagodevreede/semver/check/example/MyExampleClassTest.java
@@ -0,0 +1,15 @@
+package io.github.jagodevreede.semver.check.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyExampleClassTest {
+
+    @Test
+    void doImportantStuff() {
+        var result = new MyExampleClass().doImportantStuff();
+
+        assertTrue(result);
+    }
+}
