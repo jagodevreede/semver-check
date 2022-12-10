3fc8a919249942a371425458a7188542337b084e
Jago de Vreede <jagodevreede@gmail.com>
2022-12-10 13:05:57 +0100

    chore: change toplevel package to io.github

---
+++ sample/sample-base/src/main/java/io/github/jagodevreede/semver/sample/ExampleClass.java
@@ -0,0 +1,22 @@
+package io.github.jagodevreede.semver.sample;
+
+public class ExampleClass {
+    public static final String A_CONST = "abc";
+
+    public void aMethod() {
+
+    }
+
+    protected void aProtectedMethod() {
+        aPrivateMethod();
+    }
+
+    private void aPrivateMethod() {
+
+    }
+
+    @Deprecated
+    private void aPrivateMethodThatWillBeRemoved() {
+
+    }
+}
---
+++ semver-check-core/src/main/java/io/github/jagodevreede/semver/check/core/JarFileHelper.java
@@ -0,0 +1,11 @@
+package io.github.jagodevreede.semver.check.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.jar.JarFile;
+
+class JarFileHelper {
+    static JarFile getJarFile(final File file) throws IOException {
+        return new JarFile(file, false);
+    }
+}
